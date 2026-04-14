ad8270c3e6e3da7954d16bdac3c9d5b1856ac67e
FelipeCarballo <felipecarballo1991@gmail.com>
2026-04-14 17:16:56 +0000

    add new products

--- 01-Bronze/products.py
+++ 01-Bronze/products.py
@@ -138,8 +138,375 @@ URLS = {
                 # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
                 
                 }
-    } 
+    },
+        "marucha": {
+        "nombre": "Marucha",        
+        "unidad": "kg",        
+        "categoria": "Carnes",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/marucha-x-kg-/_/R-00043060-00043060-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "tapa_asado": {
+        "nombre": "Tapa de asado",        
+        "unidad": "kg",        
+        "categoria": "Carnes",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/tapa-de-asado-especial-x-kg-/_/R-00047861-00047861-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "asado": {
+        "nombre": "Asado",        
+        "unidad": "kg",        
+        "categoria": "Carnes",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/asado-del-medio-estancias-coto-x-kg-/_/R-00047979-00047979-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "bife_angosto": {
+        "nombre": "Bife angosto",        
+        "unidad": "kg",        
+        "categoria": "Carnes",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bife-angosto-estancias-coto-x-kg-/_/R-00047987-00047987-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "bife_ancho": {
+        "nombre": "Bife ancho",        
+        "unidad": "kg",        
+        "categoria": "Carnes",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bife-ancho-x-kg-/_/R-00041385-00041385-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "carre_cerdo": {
+        "nombre": "Carre de cerdo",        
+        "unidad": "kg",        
+        "categoria": "Cerdo",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/carre-de-cerdo-x-kg-/_/R-00017162-00017162-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "bondiola_cerdo": {
+        "nombre": "Bondiola de cerdo",        
+        "unidad": "kg",        
+        "categoria": "Cerdo",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bondiola-de-cerdo-x-kg-/_/R-00000943-00000943-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "papa_negra": {
+        "nombre": "Papa negra",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/papa-negra-seleccion-x-kg-/_/R-00060947-00060947-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "cebolla": {
+        "nombre": "Cebolla",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/cebolla-a-granel-x-kg-/_/R-00000602-00000602-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "morron_rojo": {
+        "nombre": "Morron rojo",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/pimiento-rojo-xkg-/_/R-00000671-00000671-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "zapallo_anco": {
+        "nombre": "Zapallo Anco",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/zapallo-anco-x-kg-/_/R-00000688-00000688-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "zanahoria": {
+        "nombre": "Zanahoria",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/zanahoria-seleccion-xkg-/_/R-00000686-00000686-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "tomate_redondo": {
+        "nombre": "Tomate redondo",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/tomate-red-x-kg-/_/R-00000684-00000684-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "zapallito_redondo": {
+        "nombre": "Zapallito redondo",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/zapallito-redondo-xkg-/_/R-00000691-00000691-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "brocoli": {
+        "nombre": "Brocoli",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/brocoli-x-kg-/_/R-00000598-00000598-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "cebolla_verdeo": {
+        "nombre": "Cebolla de verdeo",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/cebolla-de-verdeo-x-uni-/_/R-00046634-00046634-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "acelga": {
+        "nombre": "Acelga",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/acelga-x-uni-/_/R-00040156-00040156-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "puerro": {
+        "nombre": "Puerro",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/puerro-x-uni-/_/R-00046632-00046632-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "espinaca": {
+        "nombre": "Espinaca",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/espinaca-x-uni-/_/R-00065678-00065678-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "perejil": {
+        "nombre": "Perejil",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/perejil-x-uni-/_/R-00047599-00047599-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "ajo": {
+        "nombre": "Ajo",        
+        "unidad": "unidad",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/ajo-gigante-x-uni-/_/R-00040804-00040804-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }
+    },
+        "repollo_colorado": {
+        "nombre": "Repollo Colorado",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/repollo-colorado-x-kg-/_/R-00000680-00000680-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
      
-    
+        },
+        "coliflor": {
+        "nombre": "Coliflor",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/coliflor-x-kg-/_/R-00000619-00000619-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "lechuga_francesa": {
+        "nombre": "Lechuga francesa",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/lechuga-francesa-x-kg-/_/R-00000650-00000650-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "chacucha_rolliza": {
+        "nombre": "Chaucha rolliza",        
+        "unidad": "kg",        
+        "categoria": "Verdura",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/chaucha-rolliza-x-kg-/_/R-00000862-00000862-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "banana": {
+        "nombre": "Banana cavendish",        
+        "unidad": "kg",        
+        "categoria": "Fruta",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/banana-cavendish-x-kg-/_/R-00000446-00000446-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "naranja_jugo": {
+        "nombre": "Naranja jugo",        
+        "unidad": "kg",        
+        "categoria": "Fruta",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/naranja-jugo-xkg-/_/R-00061005-00061005-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "manzana_comercial": {
+        "nombre": "Manzana comercial",        
+        "unidad": "kg",        
+        "categoria": "Fruta",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/manzana-comercial-x-kg-/_/R-00061002-00061002-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "uva_redglobe": {
+        "nombre": "Uva redglobe",        
+        "unidad": "kg",        
+        "categoria": "Fruta",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/uva-red-globe-xkg-/_/R-00000861-00000861-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        },
+        "mandarina_okitsu": {
+        "nombre": "Mandarina okitsu",        
+        "unidad": "kg",        
+        "categoria": "Fruta",
+        "urls": {
+                "Coto": "https://www.cotodigital.com.ar/sitios/cdigi/productos/mandarina-okitsu-x-kg-/_/R-00000255-00000255-200"
+                # "Dia": "",
+                # "Jumbo": "https://www.jumbo.com.ar/pollo-fresco-con-menudos-2/p",
+                # "Carrefour": "https://www.carrefour.com.ar/pollo-entero-congelado-x-kg-699692/p"
+                
+                }                                                                                                                                             
+     
+        }                                  
 
 }
